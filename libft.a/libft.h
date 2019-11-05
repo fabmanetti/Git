f36dc91f9ddd495fb0bff6187df5dd4dfc17048e
fabmanetti <faffamanetti@gmail.com>
2019-11-05 21:32:13 +0100

    2

---
+++ libft.a/libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmanetti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/05 18:15:22 by fmanetti          #+#    #+#             */
+/*   Updated: 2019/11/05 20:43:42 by fmanetti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strlen(const char *s);
