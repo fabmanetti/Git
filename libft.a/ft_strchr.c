aa96b552f4bf26797ab9e2ea6fcae2bf7de532aa
fabmanetti <faffamanetti@gmail.com>
2019-11-06 18:44:19 +0100

    ciao

--- libft.a/ft_strchr.c
+++ libft.a/ft_strchr.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmanetti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/05 18:32:30 by fmanetti          #+#    #+#             */
-/*   Updated: 2019/11/06 18:38:03 by fmanetti         ###   ########.fr       */
+/*   Created: 2019/11/06 17:26:03 by fmanetti          #+#    #+#             */
+/*   Updated: 2019/11/06 18:07:00 by fmanetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
-int		main(int argc, char *argv[])
+char*	ft_strchr(const char *s, int c)
 {
+	int i;
+	char d;
+	char *p;
 
-	argc = 0;
-	printf("%s\n", ft_strchr(argv[1], 'c'));
-	printf("%s\n", strchr(argv[1], 'c'));
-	return (0);
+	d = (char)c;
+	i = 0;
+	p = 0;
+	while (s[i] != d || s[i] != '\0')
+		i++;
+	if (s[i] == d)
+	{
+		*p = s[i];
+		return p;
+	}
+	else
+		return (0);
 }
--- libft.a/libft.h
+++ libft.a/libft.h
@@ -6,7 +6,7 @@
 /*   By: fmanetti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 18:15:22 by fmanetti          #+#    #+#             */
-/*   Updated: 2019/11/05 20:43:42 by fmanetti         ###   ########.fr       */
+/*   Updated: 2019/11/06 17:59:11 by fmanetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char*	ft_strchr(const char *s, int c);
 int		ft_strlen(const char *s);
