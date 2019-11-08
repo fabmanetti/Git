0e4de7c35386dd6b101b06f7f0e519b8fc4b877f
fabmanetti <faffamanetti@gmail.com>
2019-11-08 20:40:57 +0100

    6

--- libft.a/ft_strlcat.c
+++ libft.a/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fmanetti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 16:24:34 by fmanetti          #+#    #+#             */
-/*   Updated: 2019/11/08 20:21:53 by fmanetti         ###   ########.fr       */
+/*   Updated: 2019/11/08 20:39:47 by fmanetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,14 @@ long	ft_strlcat(char *dst, const char *src, unsigned int size)
 	{
 		while (dst[x] != '\0')
 			x++;
-		while (src[y] != '\0' && 
+		while (src[y] != '\0' && y < size)
+		{
+			dst[x] = src[y];
+			x++;
+			y++;
+		}
+		dst[x + 1] = '\0';
 	}
+	printf("%s\n", dst);
+	return (ft_strlen(dst));
 }
