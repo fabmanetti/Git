adb4059be01a50d3ca1ca5293a90a69b27408082
fabmanetti <faffamanetti@gmail.com>
2019-11-13 17:02:07 +0100

    15

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmanetti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 16:24:34 by fmanetti          #+#    #+#             */
+/*   Updated: 2019/11/08 20:39:47 by fmanetti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_strlcat(char *dst, const char *src, unsigned int size)
+{
+	unsigned int x;
+	unsigned int y;
+	unsigned int c;
+
+	x = 0;
+	y = 0;
+	c = 0;
+	while (dst[x] != '\0')
+	{
+		x++;
+		c++;
+	}
+	if (x < size)
+	{
+		while (src[y] != '\0' && x + 1 < size)
+		{
+			dst[x] = src[y];
+			x++;
+			y++;
+		}
+		dst[x] = '\0';
+		return (c + ft_strlen(src));
+	}
+	else
+		return (size + ft_strlen(src));
+}
