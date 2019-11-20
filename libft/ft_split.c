48aeba56a857532e5f7e58a6c627087b587b041a
fabmanetti <faffamanetti@gmail.com>
2019-11-20 11:20:07 +0100

    24

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -19,12 +19,21 @@ int		count_words(const char *s, char c)
 
 	x = 0;
 	y = 0;
-	while (s[x])
+	while (s[x] != '\0')
 	{
-		if (s[x] == c)
+		if (s[x] != c)
+		{
+		printf("0");
+			while (s[x] != c && s[x] != '\0')
+				x++;
 			y++;
-		x++;
+		}
+		if (s[x] == '\0')
+			x++;
 	}
+	//if (y != 0)
+	//	y++;
+	//printf("%d\n", y);
 	return (y);
 }
 
@@ -32,21 +41,32 @@ char	*fill_array(const char *s, char c, char *t, int x)
 {
 	int		y;
 	int		z;
+	int		a;
 
+	a = x;
 	y = x;
 	z = 0;
-	while (s[x] != c)
-		x++;
-	if(!(t = malloc((x + 2) * sizeof(char))))
+	while (s[y] != c && s[y] != '\0')
+		a++;
+	if(!(t = malloc(((a - x) + 2) * sizeof(char))))
 		return (0);
-	while (z <= x && s[y] != '\0')
+	while (z < (a - x) && s[y] != '\0')
 	{
 		t[z] = s[y];
 		z++;
 		y++;
+		printf("2");
 	}
 	t[z] = '\0';
 	return (t);
+}
+
+int		advancec(const char *s, char c, int x)
+{
+	while (s[x] == c)
+		x++;
+	printf("3");
+	return (x);
 }
 
 char	**ft_split(const char *s, char c)
@@ -65,12 +85,12 @@ char	**ft_split(const char *s, char c)
 		return (0);
 	while (x < words)
 	{
+		printf("1");
 		t[x] = fill_array(s, c, t[x], y);
 		if (x != words)
-			while (s[y] != c && s[y] != '\0')
-				y++;
-		x++;
+			y = advancec(s, c, y);
 		y++;
+		x++;
 	}
 	t[x] = NULL;
 	return (t);
